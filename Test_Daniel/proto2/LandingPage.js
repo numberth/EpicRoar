f7c0fad82b5cfe46b127c9c7b323ebfd67c2eb91
Daniel Tian <daniel_tian2002@hotmail.com>
2015-05-07 11:33:49 -0700

    Adding a bit of comments

--- Test_Daniel/proto2/LandingPage.js
+++ Test_Daniel/proto2/LandingPage.js
@@ -1,3 +1,10 @@
+/**
+The track variable holds all of the sound files which
+will be used in our game. 
+
+the init function initializes track1 and track2, then starts the song by using
+playBackground();
+*/
 var track = {
 	state : 0,
     track1 : undefined,
@@ -14,16 +21,23 @@ var track = {
     }
 };
 
+/**
+*This function sets the source of the button to the page that is currently displayed
+*/
 function getButtonSrc(){
 	track.buttonSrc = document.getElementById('ayy');
 }
 
 track.init();
 
+/*
+* This function plays background music, by first detecting if track.state is equal to 0
+* since if it's equal to 1, then we stop playing the music. 
+*/
 function playBackground(){
 	if(track.state===0){
 		getButtonSrc();
-		track.buttonSrc.src ='images/button_pause.jpg';
+		track.buttonSrc.src ='images/button_audio_off.jpg';
 		switch(track.currentPage){
 			case 0:
 				track.track1.play();
@@ -52,7 +66,10 @@ function stopBackground(){
     track.state = 0;
 }
 
-
+/*
+This variable contains the string data for each of our pages, and puts them
+in the divisions.
+*/
 var pageOptions = {
 
 	reference : undefined,
@@ -60,7 +77,7 @@ var pageOptions = {
 	mainPage : "<img src='images/pic1.png' style='display:block;width:80%;height:200px;margin:auto;margin-top:9%'>" + 
 	"<button onclick='pageOptions.setPage3(this.reference)' id='playButton'>Play</button>" + 
 	"<button onclick='' id='timeModeButton'>Time Mode</button>" +
-	"<img src='images/button_pause.jpg' style='position:absolute;width:70px;height:70px;bottom:0;left:0' onclick='playBackground()' id='ayy'>",
+	"<img src='images/button_audio_off.jpg' style='position:absolute;width:70px;height:70px;bottom:0;left:0' onclick='playBackground()' id='ayy'>",
 
 	level3 : "<h3 style='font-size:29px;color:green;'>Level 3</h3>"+
         "<h3 style='font-size:29px;color:green;top:0;right:0;position:absolute'>2:00</h3>"+
@@ -72,7 +89,7 @@ var pageOptions = {
 				"<div id='threeByThree'></div><div id='threeByThree'></div><div id='threeByThree'></div><div style='clear:both'></div><div id='threeByThree'></div>"+
                 "<div id='threeByThree'></div><div id='threeByThree'></div></div></div>"+
                 "<button onclick='pageOptions.setPage()' id='playButton'>Back</button>"+
-                "<img src='images/button_pause.jpg' style='position:absolute;width:70px;height:70px;bottom:0;left:0' onclick='playBackground()' id='ayy'>",
+                "<img src='images/button_audio_off.jpg' style='position:absolute;width:70px;height:70px;bottom:0;left:0' onclick='playBackground()' id='ayy'>",
 
     init: function(id){
     	this.reference = document.getElementById(id);
@@ -93,7 +110,8 @@ var pageOptions = {
 
 }
 
-function theclick(){
-	var lol = document.getElementById('threeByThree');
-	lol.setAttribute("class","underground");
-}
+// function theclick(){
+// 	var lol = document.getElementById('threeByThree');
+// 	lol.setAttribute("class","underground");
+// }
+
