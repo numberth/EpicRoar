2430357e36e1f471d99079fd3082a1d991db4b22
Eva Yu <eva.yw.yu@gmail.com>
2015-05-14 14:17:30 -0700

    Made adjustments to the 3x3 folder in Test_Eva directory. Now accomodates a 4x4 grid and should generate a new path with every click without refreshing page.

--- Test_Eva/3x3Script/initialize3x3.js
+++ Test_Eva/3x3Script/initialize3x3.js
@@ -1,23 +1,37 @@
 /***************************************************************************************
- * The function that will call all the init functions in the program 
+ * The function that will call all the init functions in the path assignment and implementation programs  
  * 
  * 
  * @author Eva Yu
- * @version [1.0]
+ * @version [2.0]
  ***************************************************************************************
  */
 /**
  * Initializes the necessary components of the path , the path assignment, and the solution ID
  *  on load of the page 
+ *
+ * this function is called when the grid for a 3x3 page is called in the landingPage.js
  * 
  */
-function initBoard(){
+function initBoard3x3(){
   var MAX_3 = 3;
   MAX = MAX_3;
-  initPath(); // from solution_generate.js
-  assignPath3x3(); // from play_check_solution_3x3.js
+  initPath(); // from pathGenerator. js
+  assignPath3x3(); // from pathGenerator
 }
-
+ /**
+ * Initializes the necessary components of the path , the path assignment, and the solution ID
+ *  on load of the page 
+ *
+ * this function is called when the grid for a 4x4 page is called in the landingPage.js
+ * 
+ */
+ function initBoard4x4(){
+  var MAX_4 = 4;
+  MAX = MAX_4;
+  initPath();
+  assignPath4x4();
+ }
 /**
  * This function is called with every click of an image in order to rotate it and then check it against the solution
  * 
@@ -28,3 +42,9 @@ function rotateAndCheck(til){
   rotate(tile); // from play_check_solution_3x3.js
   checkUserBoard(); // from check_solution_3x3.js
 }
+
+// function rotateAndCheck4x4(til){
+//   var tile = parseInt(til); 
+//   rotate4x4(tile); // from play_check_solution_3x3.js
+//   checkUserBoard(); // from check_solution_3x3.js
+// }
